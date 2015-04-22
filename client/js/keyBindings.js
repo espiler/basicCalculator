e930de4af5d8e15f61cc620c2ec431fffd999fd3
espiler <monkeysrawesome@gmail.com>
2015-04-22 14:05:19 -0700

    clean up  keybindings

--- client/js/keyBindings.js
+++ client/js/keyBindings.js
@@ -1,157 +1,161 @@
-// var keys = {
-//   "#0": 48,
-//   "#1": 49,
-//   "#2": 50,
-//   "#3": 51,
-//   "#4": 52,
-//   "#5": 53,
-//   "#6": 54,
-//   "#7": 55,
-//   "#8": 56,
-//   "#9": 57,
-//   "#add": 43,
-//   "#subtract": 45,
-//   "#multiply": 42,
-//   "#divide": 47,
-//   "#equals": 13,
-//   "#dot": 46,
-//   "#AC": 99,
-//   "#percent": 37
-// }
+$(document).keydown(function(e){
+  switch (e.which) {
+    case 48:
+      $("#0").click().addClass('pressed');
+      break;
 
-// for (var id in keys) {
-//   $(document).keypress(function(e) {
-//     if (e.which === keys[id]) {
-//       $(id).click()
-//       .addClass('pressed');
-//       setTimeout(function() { $(id).removeClass('pressed') }, 75);
-//     }
-//   });
-// }
+    case 49:
+      $("#1").click().addClass('pressed');
+        break;
 
-$(document).keypress(function(e){
-    if (e.which === 48){
-        $("#0").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#0").removeClass('pressed') }, 75);
-    }
+    case 50:
+      $("#2").click().addClass('pressed');
+        break;
+
+    case 51:
+      $("#3").click().addClass('pressed');
+        break;
+
+    case 52:
+      $("#4").click().addClass('pressed');
+        break;
+
+    case 53:
+      $("#5").click().addClass('pressed');
+        break;
+
+    case 54:
+      $("#6").click().addClass('pressed');
+        break;
+
+    case 55:
+      $("#7").click().addClass('pressed');
+        break;
+
+    case 56:
+      $("#8").click().addClass('pressed');
+        break;
+
+    case 57:
+      $("#9").click().addClass('pressed');
+        break;
+
+    case 43:
+      $("#add").click().addClass('pressed');
+        break;
+
+    case 42:
+      $("#multiply").click().addClass('pressed');
+        break;
+
+    case 45:
+      $("#subtract").click().addClass('pressed');
+        break;
+
+    case 13:
+      $("#equals").click().addClass('pressed');
+        break;
+
+    case 61:
+      $("#equals").click().addClass('pressed');
+        break;
+
+    case 46:
+      $("#dot").click().addClass('pressed');
+        break;
+
+    case 47:
+      $("#divide").click().addClass('pressed');
+        break;
+
+    case 99:
+      $("#AC").click().addClass('pressed');
+        break;
+
+    case 37:
+      $("#percent").click().addClass('pressed');
+        break;
+  }
 });
-$(document).keypress(function(e){
-    if (e.which === 49){
-        $("#1").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#1").removeClass('pressed') }, 75);
-    }
+
+$(document).keyup(function(e){
+  switch (e.which) {
+    case 48:
+      $("#0").removeClass('pressed');
+      break;
+
+    case 49:
+      $("#1").removeClass('pressed');
+        break;
+
+    case 50:
+      $("#2").removeClass('pressed');
+        break;
+
+    case 51:
+      $("#3").removeClass('pressed');
+        break;
+
+    case 52:
+      $("#4").removeClass('pressed');
+        break;
+
+    case 53:
+      $("#5").removeClass('pressed');
+        break;
+
+    case 54:
+      $("#6").removeClass('pressed');
+        break;
+
+    case 55:
+      $("#7").removeClass('pressed');
+        break;
+
+    case 56:
+      $("#8").removeClass('pressed');
+        break;
+
+    case 57:
+      $("#9").removeClass('pressed');
+        break;
+
+    case 43:
+      $("#add").removeClass('pressed');
+        break;
+
+    case 42:
+      $("#multiply").removeClass('pressed');
+        break;
+
+    case 45:
+      $("#subtract").removeClass('pressed');
+        break;
+
+    case 13:
+      $("#equals").removeClass('pressed');
+        break;
+
+    case 61:
+      $("#equals").removeClass('pressed');
+        break;
+
+    case 46:
+      $("#dot").removeClass('pressed');
+        break;
+
+    case 47:
+      $("#divide").removeClass('pressed');
+        break;
+
+    case 99:
+      $("#AC").removeClass('pressed');
+        break;
+
+    case 37:
+      $("#percent").removeClass('pressed');
+        break;
+  }
 });
-$(document).keypress(function(e){
-    if (e.which === 50){
-        $("#2").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#2").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 51){
-        $("#3").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#3").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 52){
-        $("#4").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#4").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 53){
-        $("#5").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#5").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 54){
-        $("#6").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#6").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 55){
-        $("#7").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#7").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 56){
-        $("#8").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#8").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 57){
-        $("#9").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#9").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 43){
-        $("#add").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#add").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 42){
-        $("#multiply").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#multiply").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 45){
-        $("#subtract").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#subtract").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 13 || e.which === 61){
-        $("#equals").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#equals").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 46){
-        $("#dot").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#dot").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 47){
-        $("#divide").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#divide").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 99){
-        $("#AC").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#AC").removeClass('pressed') }, 75);
-    }
-});
-$(document).keypress(function(e){
-    if (e.which === 37){
-        $("#percent").click()
-        .addClass('pressed');
-        setTimeout(function(){ $("#percent").removeClass('pressed') }, 75);
-    }
-});
+
+
